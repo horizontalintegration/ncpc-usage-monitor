19725120d5164d23ef1b117c7ee8801c70c4dade
Shane Smyth <ssmyth@horizontal.com>
2021-04-27 17:08:01 -0500

    update logic

--- server.js
+++ server.js
@@ -18,15 +18,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async function(req, res, next) {
   try {
-      const asset = await db.query("SELECT sfid,schema_name__c FROM horizontal.asset WHERE status = 'Installed' AND active__c = 'True'");
-      console.log("Asset "+JSON.stringify(asset.rows));
+    const asset = await db.query("SELECT * FROM horizontal.asset WHERE active__c = 'True'");
+    console.log("Asset "+JSON.stringify(asset.rows));
 
+    if (asset.rows.length > 0) {
+        for(var i=0; i<asset.rows.length; i++){
+          var customerId;
+          const customerRecord = `
+            SELECT *
+            FROM public.customer
+            WHERE assetsfid = '${asset.rows[i].sfid}'
+          `;
+          const results_customerRecord = await db.query(customerRecord);
 
-      if (asset.rows.length > 0) {
-          for(var i=0; i<asset.rows.length; i++){
-            pullCustomerUsage(asset.rows[i]);
+          customerId = results_customerRecord.rows[0].id;
+
+          if(results_customerRecord.rows.length == 0){
+            // insert new customer record with details from the asset record
+            const query_insertCustomer = `
+              INSERT INTO 
+              public.customer (
+                clientId,
+                clientName,
+                dbURL,
+                clientSchema,
+                sfAccountName,
+                assetId)
+              VALUES 
+              [${asset.rows[i].sfid}, ${asset.rows[i].schema_name__c}, '', ${asset.rows[i].schema_name__c}, ${asset.rows[i].accountid}, ${asset.rows[i].sfid}]
+            `;
+            const results_insertCustomer = await client.query(query_insertCustomer);
+
+            customerId = results_insertCustomer.rows[0].id;
           }
-      }
+
+          if(customerId){
+            pullCustomerUsage(asset.rows[i], customerId);
+          }
+        }
+        console.log("All updates made.");
+        setTimeout((function() {
+            return process.exit(22);
+        }), 15000);
+    }
 
       res.status(200).send(asset.rows);
 
@@ -35,66 +69,92 @@ app.get('/', async function(req, res, next) {
   }
 })
 
-const pullCustomerUsage = async function (asset) {
+const pullCustomerUsage = async function (asset, customerId) {
   try{
-    console.log("Assets in pullCustomerUsage  "+JSON.stringify(asset));
-    console.log("Contact Schema  "+JSON.stringify(asset.schema_name__c));
-    const contacts = {"rows": [{"count":"0"}]};
-    const leads = {"rows": [{"count":"0"}]};
-    const campaignmembers = {"rows": [{"count":"0"}]};
-    const contactCheck = await db.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE  table_schema = '"+asset.schema_name__c+"' AND table_name = 'contact')");
-    const leadCheck = await db.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE  table_schema = '"+asset.schema_name__c+"' AND table_name = 'lead')");
-    const campaignCheck = await db.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE  table_schema = '"+asset.schema_name__c+"' AND table_name = 'campaignmember')");
+      //const tableArray = ["contact", "lead", "campaignmember", "ncpc__pc_subscription__c", "ncpc__pc_interest__c", "ncpc__pc_summary__c", "ncpc__pc_result__c"];
+
+      const contacts = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".contact");
+      const leads = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".lead");
+      const campaignmembers = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".campaignmember");
+      const subscriptions = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_subscription__c");
+      const interests = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_interest__c");
+      const summary = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_summary__c");
+      const result = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_result__c");
+
+      const totalUsage = Number(subscriptions.rows[0].count) + Number(interests.rows[0].count) + Number(contacts.rows[0].count) + Number(leads.rows[0].count) + Number(campaignmembers.rows[0].count) + Number(summary.rows[0].count) + Number(result.rows[0].count);
+      console.log("Total Usage "+JSON.stringify(totalUsage));
+
+      const tableValues = {
+        "contacts": Number(contacts.rows[0].count), 
+        "leads": Number(contacts.rows[0].count),
+        "campaignmembers": Number(campaignmembers.rows[0].count),
+        "subscriptions": Number(subscriptions.rows[0].count),
+        "summary": Number(summary.rows[0].count),
+        "result": Number(results.rows[0].count),
+        "total": totalUsage
+      };
     
-    if(contactCheck.rows[0].exists == 'true'){
-      const contacts = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_subscription__c");
-    }
-    if(leadCheck.rows[0].exists == 'true'){
-      const leads = db.query("SELECT sfid FROM "+asset.schema_name__c+".lead");
-    }
-    if(campaignCheck.rows[0].exists == 'true'){
-      const campaignmembers = db.query("SELECT sfid FROM "+asset.schema_name__c+".campaignmember");
-    }
+    //console.log("Subscriptions Count - "+asset.schema_name__c+" - "+JSON.stringify(subscriptions.rows[0].count));
 
-    const subscriptions = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_subscription__c");
-    const interests = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_interest__c");
-
-    console.log("Subscriptions - "+asset.schema_name__c+" - "+JSON.stringify(subscriptions.rows));
-    console.log("Subscriptions Count - "+asset.schema_name__c+" - "+JSON.stringify(subscriptions.rows[0].count));
-
-    const totalUsage = Number(subscriptions.rows[0].count) + Number(interests.rows[0].count) + Number(contacts.rows[0].count) + Number(leads.rows[0].count) + Number(campaignmembers.rows[0].count);
-    console.log("Total Usage "+JSON.stringify(totalUsage));
-
-    const record = await db.query("SELECT * FROM ncpc_usage.customer_usage WHERE sfid = '"+asset.sfid+"'");
-    if(record.rows.length > 0){
-        console.log("Inside update");
-        const result = await db.query(
-            "UPDATE ncpc_usage.customer_usage SET total_usage=$7, contact_table=$1, lead_table=$2, subscription_table=$3, interest_table=$4, campaignmember_table=$5 WHERE sfid=$6 RETURNING *",
-            [contacts.rows[0].count, leads.rows[0].count, subscriptions.rows[0].count, interests.rows[0].count, campaignmembers.rows[0].count, asset.sfid, totalUsage]
-        );
-        if(result.rows){updateAssetUsage(asset, totalUsage);}
-    }else{
-        console.log("Inside insert");
-        const result = await db.query(
-          "INSERT INTO ncpc_usage.customer_usage (sfid,name,subscription_table,interest_table,contact_table,lead_table,campaignmember_table,total_usage) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
-          [asset.sfid, asset.schema_name__c, Number(subscriptions.rows[0].count), Number(interests.rows[0].count), Number(contacts.rows[0].count), Number(leads.rows[0].count), Number(campaignmembers.rows[0].count), totalUsage]
-      );
-        if(result.rows){updateAssetUsage(asset, totalUsage);}
-    }
+    const updateCustomer = updateCustomerUsage(asset, tableValues);
+    const insertCustomerSnapshot = insertCustomerUsageSnapshot(asset, tableValues);
+    
   }catch(err){
     console.log("Error  "+JSON.stringify(err));
   }
 }
 
-const updateAssetUsage = async function (asset, totalUsage){
-  var today = dateFormat(new Date(), "yyyy-mm-dd");
-  //var externalKey = asset.ncpc__external_id__c === '' ? uuidv1() : asset.ncpc__external_id__c;
+const updateCustomerUsage = async function (asset, tableValues){
   try{
-    const updateAsset = db.query(
-      "UPDATE horizontal.asset SET current_volume__c=$1, usage_updated_date__c=$2 WHERE sfid=$3 RETURNING *",
-      [totalUsage, today, asset.sfid]
+    const update_customerUsage = await db.query(
+        "UPDATE ncpc_usage.customer_usage SET contact_table=$1, lead_table=$2, subscription_table=$3, interest_table=$4, campaignmember_table=$5, summary_table=$6, result_table=$7 total_usage=$8 WHERE sfid=$9 RETURNING *",
+        [tableValues.contacts, tableValues.leads, tableValues.subscriptions, tableValues.interests, tableValues.campaignmembers, tableValues.summary, tableValues.result, tableValues.total, asset.sfid]
     );
-    console.log("Update Asset: "+JSON.stringify(updateAsset));
+    const results_customerUsage = await client.query(update_customerUsage);
+
+    if(results_customerUsage.rows){updateAssetUsage(asset, tableValues);}
+    console.log("DEBUG updateCustomerUsage ",results_customerUsage);
+  }catch(err){
+    console.log("Error  "+JSON.stringify(err));
+  }
+}
+
+const insertCustomerUsageSnapshot = async function (asset, tableValues){
+  try{
+    const query_insertSnapshot = `
+        INSERT INTO 
+        public.customer_usage_snapshot (
+          sfid,
+          name,
+          subscription_table,
+          interest_table,
+          contact_table,
+          lead_table,
+          campaignmember_table,
+          summary_table,
+          result_table,
+          total_usage)
+        VALUES 
+        (${asset.sfid}, ${asset.schema_name__c}, ${tableValues.subscriptions}, ${tableValues.interests}, ${tableValues.contacts}, ${tableValues.leads}, ${tableValues.campaignmembers}, ${tableValues.summary}, ${tableValues.result}, ${tableValues.total})
+      `;
+    const results_insertSnapshot = await client.query(query_insertSnapshot);
+    console.log("DEBUG insertCustomerUsageSnapshot ",results_insertSnapshot);
+  }catch(err){
+    console.log("Error  "+JSON.stringify(err));
+  }
+}
+
+const updateAssetUsage = async function (asset, tableValues){
+  try{
+    var today = dateFormat(new Date(), "yyyy-mm-dd");
+
+    const update_asset = db.query(
+      "UPDATE horizontal.asset SET current_volume__c=$1, usage_updated_date__c=$2 WHERE sfid=$3 RETURNING *",
+      [tableValues.total, today, asset.sfid]
+    );
+    const results_asset = await client.query(update_asset);  
+
+    console.log("DEBUG updateCustomerUsage ",results_asset);
   }catch(err){
     console.log("Error  "+JSON.stringify(err));
   } 
--- usage-update.js
+++ usage-update.js
@@ -2,13 +2,43 @@ const db = require("./db");
 const dateFormat = require('dateformat');
 
 const getAssetRecords = async function (){
-    const asset = await db.query("SELECT sfid,schema_name__c FROM horizontal.asset WHERE status = 'Installed' AND active__c = 'True'");
+    const asset = await db.query("SELECT * FROM horizontal.asset WHERE active__c = 'True'");
       console.log("Asset "+JSON.stringify(asset.rows));
-
 
       if (asset.rows.length > 0) {
           for(var i=0; i<asset.rows.length; i++){
-            pullCustomerUsage(asset.rows[i]);
+            var customerId;
+            const customerRecord = `
+              SELECT *
+              FROM public.customer
+              WHERE assetsfid = '${asset.rows[i].sfid}'
+            `;
+            const results_customerRecord = await db.query(customerRecord);
+
+            customerId = results_customerRecord.rows[0].id;
+
+            if(results_customerRecord.rows.length == 0){
+              // insert new customer record with details from the asset record
+              const query_insertCustomer = `
+                INSERT INTO 
+                public.customer (
+                  clientId,
+                  clientName,
+                  dbURL,
+                  clientSchema,
+                  sfAccountName,
+                  assetId)
+                VALUES 
+                [${asset.rows[i].sfid}, ${asset.rows[i].schema_name__c}, '', ${asset.rows[i].schema_name__c}, ${asset.rows[i].accountid}, ${asset.rows[i].sfid}]
+              `;
+              const results_insertCustomer = await client.query(query_insertCustomer);
+
+              customerId = results_insertCustomer.rows[0].id;
+            }
+
+            if(customerId){
+              pullCustomerUsage(asset.rows[i], customerId);
+            }
           }
           console.log("All updates made.");
           setTimeout((function() {
@@ -17,63 +47,92 @@ const getAssetRecords = async function (){
       }
 }
 
-const pullCustomerUsage = async function (asset) {
+const pullCustomerUsage = async function (asset, customerId) {
     try{
-      //console.log("Assets in pullCustomerUsage  "+JSON.stringify(asset));
-      //console.log("Contact Schema  "+JSON.stringify(asset.schema_name__c));
-      /*const contacts = {"rows": [{"count":"0"}]};
-      const leads = {"rows": [{"count":"0"}]};
-      const campaignmembers = {"rows": [{"count":"0"}]};
-      const subscriptions = {"rows": [{"count":"0"}]};
-      const interests = {"rows": [{"count":"0"}]};
-      const contactCheck = await db.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE  table_schema = '"+asset.schema_name__c+"' AND table_name = 'contact')");
-      const leadCheck = await db.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE  table_schema = '"+asset.schema_name__c+"' AND table_name = 'lead')");
-      const campaignCheck = await db.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE  table_schema = '"+asset.schema_name__c+"' AND table_name = 'campaignmember')");
-      const subsCheck = await db.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE  table_schema = '"+asset.schema_name__c+"' AND table_name = 'ncpc__pc_subscription__c')");
-      const intsCheck = await db.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE  table_schema = '"+asset.schema_name__c+"' AND table_name = 'ncpc__pc_interest__c')");
-      */
+        //const tableArray = ["contact", "lead", "campaignmember", "ncpc__pc_subscription__c", "ncpc__pc_interest__c", "ncpc__pc_summary__c", "ncpc__pc_result__c"];
+
         const contacts = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".contact");
-      
         const leads = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".lead");
-      
         const campaignmembers = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".campaignmember");
-      
         const subscriptions = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_subscription__c");
-      
         const interests = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_interest__c");
+        const summary = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_summary__c");
+        const result = await db.query("SELECT count(*) FROM "+asset.schema_name__c+".ncpc__pc_result__c");
+
+        const totalUsage = Number(subscriptions.rows[0].count) + Number(interests.rows[0].count) + Number(contacts.rows[0].count) + Number(leads.rows[0].count) + Number(campaignmembers.rows[0].count) + Number(summary.rows[0].count) + Number(result.rows[0].count);
+        console.log("Total Usage "+JSON.stringify(totalUsage));
+
+        const tableValues = {
+          "contacts": Number(contacts.rows[0].count), 
+          "leads": Number(contacts.rows[0].count),
+          "campaignmembers": Number(campaignmembers.rows[0].count),
+          "subscriptions": Number(subscriptions.rows[0].count),
+          "summary": Number(summary.rows[0].count),
+          "result": Number(results.rows[0].count),
+          "total": totalUsage
+        };
       
       //console.log("Subscriptions Count - "+asset.schema_name__c+" - "+JSON.stringify(subscriptions.rows[0].count));
   
-      const totalUsage = Number(subscriptions.rows[0].count) + Number(interests.rows[0].count) + Number(contacts.rows[0].count) + Number(leads.rows[0].count) + Number(campaignmembers.rows[0].count);
-      console.log("Total Usage "+JSON.stringify(totalUsage));
-  
-      const record = await db.query("SELECT * FROM ncpc_usage.customer_usage WHERE sfid = '"+asset.sfid+"'");
-      if(record.rows.length > 0){
-          const result = await db.query(
-              "UPDATE ncpc_usage.customer_usage SET total_usage=$7, contact_table=$1, lead_table=$2, subscription_table=$3, interest_table=$4, campaignmember_table=$5 WHERE sfid=$6 RETURNING *",
-              [contacts.rows[0].count, leads.rows[0].count, subscriptions.rows[0].count, interests.rows[0].count, campaignmembers.rows[0].count, asset.sfid, totalUsage]
-          );
-          if(result.rows){updateAssetUsage(asset, totalUsage);}
-      }else{
-          const result = await db.query(
-            "INSERT INTO ncpc_usage.customer_usage (sfid,name,subscription_table,interest_table,contact_table,lead_table,campaignmember_table,total_usage) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
-            [asset.sfid, asset.schema_name__c, Number(subscriptions.rows[0].count), Number(interests.rows[0].count), Number(contacts.rows[0].count), Number(leads.rows[0].count), Number(campaignmembers.rows[0].count), totalUsage]
-        );
-          if(result.rows){updateAssetUsage(asset, totalUsage);}
-      }
+      const updateCustomer = updateCustomerUsage(asset, tableValues);
+      const insertCustomerSnapshot = insertCustomerUsageSnapshot(asset, tableValues);
+      
     }catch(err){
       console.log("Error  "+JSON.stringify(err));
     }
   }
   
-  const updateAssetUsage = async function (asset, totalUsage){
-    console.log("Will update "+JSON.stringify(asset.schema_name__c)+" to "+JSON.stringify(totalUsage));
-    var today = dateFormat(new Date(), "yyyy-mm-dd");
+  const updateCustomerUsage = async function (asset, tableValues){
     try{
-      const result = db.query(
-        "UPDATE horizontal.asset SET current_volume__c=$1, usage_updated_date__c=$2 WHERE sfid=$3 RETURNING *",
-        [totalUsage, today, asset.sfid]
+      const update_customerUsage = await db.query(
+          "UPDATE ncpc_usage.customer_usage SET contact_table=$1, lead_table=$2, subscription_table=$3, interest_table=$4, campaignmember_table=$5, summary_table=$6, result_table=$7 total_usage=$8 WHERE sfid=$9 RETURNING *",
+          [tableValues.contacts, tableValues.leads, tableValues.subscriptions, tableValues.interests, tableValues.campaignmembers, tableValues.summary, tableValues.result, tableValues.total, asset.sfid]
       );
+      const results_customerUsage = await client.query(update_customerUsage);
+
+      if(results_customerUsage.rows){updateAssetUsage(asset, tableValues);}
+      console.log("DEBUG updateCustomerUsage ",results_customerUsage);
+    }catch(err){
+      console.log("Error  "+JSON.stringify(err));
+    }
+  }
+
+  const insertCustomerUsageSnapshot = async function (asset, tableValues){
+    try{
+      const query_insertSnapshot = `
+          INSERT INTO 
+          public.customer_usage_snapshot (
+            sfid,
+            name,
+            subscription_table,
+            interest_table,
+            contact_table,
+            lead_table,
+            campaignmember_table,
+            summary_table,
+            result_table,
+            total_usage)
+          VALUES 
+          (${asset.sfid}, ${asset.schema_name__c}, ${tableValues.subscriptions}, ${tableValues.interests}, ${tableValues.contacts}, ${tableValues.leads}, ${tableValues.campaignmembers}, ${tableValues.summary}, ${tableValues.result}, ${tableValues.total})
+        `;
+      const results_insertSnapshot = await client.query(query_insertSnapshot);
+      console.log("DEBUG insertCustomerUsageSnapshot ",results_insertSnapshot);
+    }catch(err){
+      console.log("Error  "+JSON.stringify(err));
+    }
+  }
+
+  const updateAssetUsage = async function (asset, tableValues){
+    try{
+      var today = dateFormat(new Date(), "yyyy-mm-dd");
+
+      const update_asset = db.query(
+        "UPDATE horizontal.asset SET current_volume__c=$1, usage_updated_date__c=$2 WHERE sfid=$3 RETURNING *",
+        [tableValues.total, today, asset.sfid]
+      );
+      const results_asset = await client.query(update_asset);  
+
+      console.log("DEBUG updateCustomerUsage ",results_asset);
     }catch(err){
       console.log("Error  "+JSON.stringify(err));
     } 
