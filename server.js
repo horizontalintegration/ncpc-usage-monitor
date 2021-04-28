e2cfefc6a2b763d1b2c3a95380a6bae970cbd800
Shane Smyth <ssmyth@horizontal.com>
2021-04-27 22:33:58 -0500

    update

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async function(req, res, next) {
   try {
+    console.log("test");
     const asset = await db.internaldb.query("SELECT * FROM horizontal.asset WHERE active__c = 'True'");
-      console.log("Asset "+JSON.stringify(asset.rows));
+    console.log("Asset "+JSON.stringify(asset.rows));
 
       if (asset.rows.length > 0) {
           for(var i=0; i<asset.rows.length; i++){
