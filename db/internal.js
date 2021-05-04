47181c8f76edef2fd4cfa6b89d8c185b4cfe8822
Shane Smyth <ssmyth@horizontalintegration.com>
2021-05-04 10:19:32 -0500

    update

--- db/internal.js
+++ db/internal.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: 'postgres://tfbyjlcnynawyh:7f961d7691be42ab2f7187ab3f03b770c55a1626466190929da6d7fb99d73596@ec2-23-21-229-200.compute-1.amazonaws.com:5432/dd8q3btk67ctro',
     ssl: {
         rejectUnauthorized: false
     }
